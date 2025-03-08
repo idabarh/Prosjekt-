00895cfb3a89c6e85147b94252f4cddb4ef80bde
Idabarh <idamba@uia.no>
2025-03-08 16:44:09 +0100

    laget en middleware for å lagre session info

--- server.mjs
+++ server.mjs
@@ -1,11 +1,22 @@
 import express from 'express'
 import HTTP_CODES from "./utils/httpCodes.mjs";
+import middleware from "./Modules/middleware.mjs";
+
+
 
 const server = express();
 const port = (process.env.PORT || 8000);
 
 server.set('port', port);
+
+server.use(middleware);
 server.use(express.static('public'));
+
+server.get('/test-session', (req, res) => {
+    req.session.visits = (req.session.visits || 0) + 1;
+    req.saveSession();
+    res.send(`Antall besøk: ${req.session.visits}`);
+});
 
 const decks = {};
 
