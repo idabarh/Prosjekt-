04b359e7a3ba3277182fb0b8a5d93baa93b43c11
Idabarh <idamba@uia.no>
2025-03-08 13:42:40 +0100

    laget et poem

--- server.mjs
+++ server.mjs
@@ -1,18 +1,31 @@
 import express from 'express'
 import HTTP_CODES from "./utils/httpCodes.mjs";
- 
- const server = express();
- const port = (process.env.PORT || 8000);
- 
- server.set('port', port);
- server.use(express.static('public'));
- 
- function getRoot(req, res, next) {
-     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
- }
- 
- server.get("/", getRoot);
- 
- server.listen(server.get('port'), function () {
-     console.log('server running', server.get('port'));
- });
+
+const server = express();
+const port = (process.env.PORT || 8000);
+
+server.set('port', port);
+server.use(express.static('public'));
+
+function getRoot(req, res, next) {
+    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
+}
+
+function getPoem(req, res, next) {
+
+    const poem = `
+    Mørket hvisker, lyset ler,
+    natten skjuler det dagen ser.
+    Stjerner blinker, vinden går,
+    tiden stanser – men hjertet slår.`;
+
+    res.status(HTTP_CODES.SUCCESS.OK).send(`${poem}`).end();
+}
+
+
+server.get("/", getRoot);
+server.get("/tmp/poem", getPoem);
+
+server.listen(server.get('port'), function () {
+    console.log('server running', server.get('port'));
+});
